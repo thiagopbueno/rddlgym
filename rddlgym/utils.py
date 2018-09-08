36986860398c1cfb9bcad6635df5ec69a01295c7
Thiago P. Bueno <thiago.pbueno@gmail.com>
2018-09-08 18:36:54 -0400

    Add missing imports in utils.py

--- rddlgym/utils.py
+++ rddlgym/utils.py
@@ -13,8 +13,10 @@
 # You should have received a copy of the GNU General Public License
 # along with rddlgym. If not, see <http://www.gnu.org/licenses/>.
 
-
+import pyrddl
 from pyrddl.parser import RDDLParser
+
+import tfrddlsim
 from tfrddlsim.rddl2tf.compiler import Compiler
 
 import rddlgym
