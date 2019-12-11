03645e32dbbb9f6f14f64633af7baf1f2e2c69b5
Thiago P. Bueno <thiago.pbueno@gmail.com>
2019-12-11 17:33:15 -0300

    chore: update install_requires in setup.py and requirements.txt
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- setup.py
+++ setup.py
@@ -1,40 +1,55 @@
+# This file is part of rddlgym.
+
+# rddlgym is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# rddlgym is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with rddlgym. If not, see <http://www.gnu.org/licenses/>.
+
+# pylint: disable=missing-docstring
+
+
 import os
 from setuptools import setup, find_packages
 
 
 def read(filename):
     filepath = os.path.join(os.path.dirname(__file__), filename)
-    file = open(filepath, 'r')
+    file = open(filepath, "r")
     return file.read()
 
 
 setup(
-    name='rddlgym',
-    version=read('version.txt'),
-    author='Thiago P. Bueno',
-    author_email='thiago.pbueno@gmail.com',
-    description='rddlgym: A toolkit for working with RDDL domains in Python3.',
-    long_description=read('README.md'),
-    license='GNU General Public License v3.0',
-    keywords=['rddl', 'toolkit'],
-    url='https://github.com/thiagopbueno/rddlgym',
+    name="rddlgym",
+    version=read("version.txt"),
+    author="Thiago P. Bueno",
+    author_email="thiago.pbueno@gmail.com",
+    description="rddlgym: A toolkit for working with RDDL domains in Python3.",
+    long_description=read("README.md"),
+    license="GNU General Public License v3.0",
+    keywords=["rddl", "toolkit"],
+    url="https://github.com/thiagopbueno/rddlgym",
     packages=find_packages(),
-    scripts=['scripts/rddlgym'],
-    python_requires='>=3.5',
-    install_requires=[
-        'pyrddl>=0.1.7',
-        'rddl2tf>=0.4.7'
-    ],
+    scripts=["scripts/rddlgym"],
+    python_requires=">=3.5",
+    install_requires=["pyrddl", "rddl2tf"],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
